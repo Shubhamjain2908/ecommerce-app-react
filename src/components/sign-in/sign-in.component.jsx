e24c446f6e215f20de199a8ca42412455511590d
Shubham Jain <shubhamjain2908@gmail.com>
2020-11-21 21:32:00 +0530

    sign up component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,7 +50,7 @@ class SignIn extends React.Component {
                         required
                     />
                     <CustomButton type='submit'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                    <CustomButton onClick={signInWithGoogle}>
                         Sign in with Google
                     </CustomButton>
                 </form>
