d0117e3804a94fe701bd3c7186c42f5f60e5e1cb
Shubham Jain <shubhamjain2908@gmail.com>
2020-11-22 14:31:52 +0530

    cart icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import { CartIcon } from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -26,7 +27,9 @@ const Header = ({ currentUser }) => (
                         <Link className='option' to='/signin'>
                             SIGN IN
                         </Link>
-                    )}
+                    )
+            }
+            <CartIcon />
         </div>
     </div >
 );
