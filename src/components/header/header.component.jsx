024a094e711310f7d8f06b203fc1259905154856
Shubham Jain <shubhamjain2908@gmail.com>
2020-11-22 14:49:08 +0530

    implementing redux in cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { CartIcon } from '../cart-icon/cart-icon.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header' >
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -32,12 +32,12 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        {hidden ? null : <CartDropdown />}
     </div >
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser, hidden
 });
 
 export default connect(mapStateToProps)(Header);
