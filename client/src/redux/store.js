f6991349ab5a9a304c4ee4aea89a481964282791
Shubham Jain <shubhamjain2908@gmail.com>
2020-12-04 00:33:08 +0530

    added apollo client & graphql dependencies along with inital setup

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
+import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
-
+import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const middlewares = [];
@@ -10,8 +9,8 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+const persistor = persistStore(store);
 
-export default { store, persistStore };
+export { store, persistor };
