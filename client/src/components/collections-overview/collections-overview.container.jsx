f36d5b8b44acea8b2fff19fdd4df2039cc33d742
Shubham Jain <shubhamjain2908@gmail.com>
2020-12-08 18:57:08 +0530

    added apollo container

---
+++ client/src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,34 @@
+import { gql } from 'apollo-boost';
+import React from 'react';
+import { Query } from 'react-apollo';
+import Spinner from '../spinner/spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+
+const GET_COLLECTIONS = gql`
+    {
+        collections {
+            id
+            title
+            items {
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`;
+
+const CollectionsOverviewContainer = () => (
+    <Query query={GET_COLLECTIONS}>
+        {
+            ({ loading, data }) => {
+                if (loading) return <Spinner />;
+                return <CollectionsOverview collections={data.collections}/>
+            }
+        }
+    </Query>
+);
+
+export default CollectionsOverviewContainer;
