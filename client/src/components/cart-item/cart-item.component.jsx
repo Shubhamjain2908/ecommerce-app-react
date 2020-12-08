77e481cce4ace86136abeb258b08820d03462a5c
Shubham Jain <shubhamjain2908@gmail.com>
2020-12-09 02:06:39 +0530

    react memo in cart-item

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import {
   CartItemContainer,
-  ItemDetailsContainer,
-  CartItemImage
+
+  CartItemImage, ItemDetailsContainer
 } from './cart-item.styles';
+
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <CartItemContainer>
@@ -18,4 +18,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   </CartItemContainer>
 );
 
-export default CartItem;
+export default React.memo(CartItem);
